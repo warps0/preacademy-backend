60da310607bf9aca535d0297ad62d6341aafb1fe
warps0 <matias.trossero.1@gmail.com>
2025-08-01 11:06:10 -0300

    Segunda Evaluacion - Inicialización del proyecto Configuración inicial del proyecto
    Version de Java: 17
    Version de Spring Boot: 3.5.4
    
    Dependencias:
    - Spring Web
    - Spring Devtools
    
    Paquetes:
    - model
    - controller
    - service
    - repository
    - exception

---
+++ src/segunda_evaluacion/src/main/java/com/warps/segunda_evaluacion/segunda_evaluacion/SegundaEvaluacionApplication.java
@@ -0,0 +1,13 @@
+package com.warps.segunda_evaluacion.segunda_evaluacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundaEvaluacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundaEvaluacionApplication.class, args);
+	}
+
+}
---
+++ src/segunda_evaluacion/src/test/java/com/warps/segunda_evaluacion/segunda_evaluacion/SegundaEvaluacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warps.segunda_evaluacion.segunda_evaluacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundaEvaluacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
