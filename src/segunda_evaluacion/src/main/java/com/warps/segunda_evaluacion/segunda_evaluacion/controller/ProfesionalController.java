c823da0a8ca695ec65372efe7347af74b2270dfa
warps0 <matias.trossero.1@gmail.com>
2025-08-05 11:42:05 -0300

    Controlador de Profesional Controlador genérico para manejar las operaciones CRUD de la entidad Profesional.
    TODO en próximo commit:
        - Listar por especialidad

---
+++ src/segunda_evaluacion/src/main/java/com/warps/segunda_evaluacion/segunda_evaluacion/controller/ProfesionalController.java
@@ -0,0 +1,45 @@
+package com.warps.segunda_evaluacion.segunda_evaluacion.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.warps.segunda_evaluacion.segunda_evaluacion.model.Profesional;
+import com.warps.segunda_evaluacion.segunda_evaluacion.service.ProfesionalService;
+
+@RestController
+@RequestMapping("/profesionales")
+public class ProfesionalController {
+    @Autowired
+    ProfesionalService profesionalService;
+
+    @GetMapping
+    public List<Profesional> findAll() {
+        return profesionalService.findAll();
+    }
+    
+    @GetMapping("/{id}")
+    public Profesional findById(@PathVariable Long id) {
+        // Si el id no es válido, se lanza una excepción
+        return profesionalService.findById(id);
+    }
+    
+    @PostMapping
+    public Profesional save(@RequestBody Profesional entity) {
+        // Si algun dato no es válido, se lanza una excepción
+        return profesionalService.save(entity);
+    }
+    
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        // Si el id no es válido, se lanza una excepción
+        profesionalService.deleteById(id);
+    }
+}
