f809dbe01db830c715d76b64b1c88fc7ac28b33b
warps0 <matias.trossero.1@gmail.com>
2025-08-06 06:55:31 -0300

    Excepciones Personalizadas Se crearon las clases de excepciones sugeridas en el enunciado.

---
+++ src/segunda_evaluacion/src/main/java/com/warps/segunda_evaluacion/segunda_evaluacion/exception/RecursoNoEncontradoException.java
@@ -0,0 +1,7 @@
+package com.warps.segunda_evaluacion.segunda_evaluacion.exception;
+
+public class RecursoNoEncontradoException extends RuntimeException {
+    RecursoNoEncontradoException(String message){
+        super(message);
+    }
+}
